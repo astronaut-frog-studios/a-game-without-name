31832cc359aec3ea5c696a395e999cf47c0f56fd
murilinhops <murilinhorps@gmail.com>
2022-06-23 16:48:41 -0300

    Create a limit for the level objects

---
+++ Assets/Scripts/Utils/LevelBounds.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBounds : StaticInstance<LevelBounds>
+{
+    [SerializeField] private MeshCollider spawnArea;
+    public static float minBoundsX { get; private set; }
+    public static float maxBoundsX { get; private set; }
+
+    public static float minBoundsY { get; private set; }
+    public static float maxBoundsY { get; private set; }
+
+    public static Vector2 maxBounds { get; private set; }
+    public static Vector2 minBounds { get; private set; }
+
+
+    private void Start()
+    {
+        spawnArea = GetComponent<MeshCollider>();
+
+        minBoundsX = spawnArea.bounds.min.x;
+        maxBoundsX = spawnArea.bounds.max.x;
+        minBoundsY = spawnArea.bounds.min.y;
+        maxBoundsY = spawnArea.bounds.max.y;
+
+        maxBounds = spawnArea.bounds.max;
+        minBounds = spawnArea.bounds.min;
+    }
+
+    public static float RandomPosX => Random.Range(LevelBounds.minBoundsX, LevelBounds.maxBoundsX);
+    public static float RandomPosY => Random.Range(LevelBounds.minBoundsY, LevelBounds.maxBoundsY);
+
+}
