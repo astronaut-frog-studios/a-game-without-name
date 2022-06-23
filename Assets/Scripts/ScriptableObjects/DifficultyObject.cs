7685eddfc73b3afcea5c3cd33608b84f285eeed4
murilinhops <murilinhorps@gmail.com>
2022-06-23 18:01:20 -0300

    Add difficulty for enemies and some player stats (damage and health)

--- Assets/Scripts/ScriptableObjects/DifficultyObject.cs
+++ Assets/Scripts/ScriptableObjects/DifficultyObject.cs
@@ -3,12 +3,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Difficulty_SO", menuName = "ScriptableObjects/Difficulty")]
 public class DifficultyObject : ScriptableObject
 {
+    [Header("Waves")]
     public int numberOfEnemiesMultiplier = 2;
     public int numberOfEnemies = 2;
-    
     public float spawnRateMultiplier = 0.04f;
     public float spawnRate = 0.16f;
-
     public int specialRoundsNumber = 4;
     public int specialWaveLenght = 3;
+
+    [Header("Enemies")]
+    public float enemyHealthMultiplier = 2f;
+    public float enemyHealth = 0;
+    public float enemyDamageMultiplier = 0.6f;
+    public float enemyDamage = 0;
+    public float enemyCooldownMultiplier = 0.2f;
+    public float enemyCooldown = 0;
+    public float enemyMinCooldown = 1.2f;
+
+    [Header("Player")]
+    public float playerHealthMultiplier = 0.4f;
+    public float playeryHealth = 0;
+    public float playerDamageMultiplier = 0.2f;
+    public float playerDamage = 0;
 }
--- Assets/Scripts/Utils/Difficulty.cs
+++ Assets/Scripts/Utils/Difficulty.cs
@@ -4,23 +4,38 @@ public class Difficulty : PersistentSingleton<Difficulty>
 {
     [SerializeField] private DifficultyObject difficultyObject;
 
+    [Header("Waves")]
     [ReadOnly] public int numberOfEnemiesMultiplier;
-    [ReadOnly] public float spawnRateMultiplier;
-
     [ReadOnly, SerializeField] private int numberOfEnemies;
-    [ReadOnly, SerializeField] private float spawnRate;
-
     [HideInInspector] public int specialRoundsNumber;
     [HideInInspector] public int specialWaveLenght;
-    
+    [ReadOnly] public float spawnRateMultiplier;
+    [ReadOnly, SerializeField] private float spawnRate;
+
+    [Header("Enemies")]
+    [HideInInspector] public float enemyHealthMultiplier;
+    [ReadOnly, SerializeField] public float enemyHealth;
+    [HideInInspector] public float enemyDamageMultiplier;
+    [ReadOnly, SerializeField] public float enemyDamage;
+    [HideInInspector] public float enemyCooldownMultiplier;
+    [ReadOnly, SerializeField] public float enemyCooldown;
+
+    [Header("Player")]
+    [HideInInspector] public float playerHealthMultiplier;
+    [ReadOnly, SerializeField] public float playeryHealth;
+    [HideInInspector] public float playerDamageMultiplier;
+    [ReadOnly, SerializeField] public float playerDamage;
+
     public float SpawnRateDifficulty => spawnRate += spawnRateMultiplier;
     public int EnemiesNumberDifficulty => numberOfEnemies += numberOfEnemiesMultiplier;
-    
+
     private void Start()
     {
         SetNumberOfEnemies();
         SetSpawnRate();
         SetSpecialWave();
+        SetEnemies();
+        SetPlayer();
     }
 
     private void SetNumberOfEnemies()
@@ -41,12 +56,41 @@ public class Difficulty : PersistentSingleton<Difficulty>
         specialWaveLenght = difficultyObject.specialWaveLenght;
     }
 
+    private void SetEnemies()
+    {
+        enemyDamageMultiplier = difficultyObject.enemyDamageMultiplier;
+        enemyDamage = difficultyObject.enemyDamage;
+        enemyHealthMultiplier = difficultyObject.enemyHealthMultiplier;
+        enemyHealth = difficultyObject.enemyHealth;
+        enemyCooldownMultiplier = difficultyObject.enemyCooldownMultiplier;
+        enemyCooldown = difficultyObject.enemyCooldown;
+    }
+
+    private void SetPlayer()
+    {
+        playerHealthMultiplier = difficultyObject.playerHealthMultiplier;
+        playeryHealth = difficultyObject.playeryHealth;
+        playerDamageMultiplier = difficultyObject.playerDamageMultiplier;
+        playerDamage = difficultyObject.playerDamage;
+    }
+
     public void OnSave()
     {
         difficultyObject.numberOfEnemiesMultiplier = numberOfEnemiesMultiplier;
         difficultyObject.numberOfEnemies = numberOfEnemies;
-
         difficultyObject.spawnRateMultiplier = spawnRateMultiplier;
         difficultyObject.spawnRate = spawnRate;
+
+        difficultyObject.enemyDamageMultiplier = enemyDamageMultiplier;
+        difficultyObject.enemyDamage = enemyDamage;
+        difficultyObject.enemyHealthMultiplier = enemyHealthMultiplier;
+        difficultyObject.enemyHealth = enemyHealth;
+        difficultyObject.enemyCooldownMultiplier = enemyCooldownMultiplier;
+        difficultyObject.enemyCooldown = enemyCooldown;
+
+        difficultyObject.playerHealthMultiplier = playerHealthMultiplier;
+        difficultyObject.playeryHealth = playeryHealth;
+        difficultyObject.playerDamageMultiplier = playerDamageMultiplier;
+        difficultyObject.playerDamage = playerDamage;
     }
 }
