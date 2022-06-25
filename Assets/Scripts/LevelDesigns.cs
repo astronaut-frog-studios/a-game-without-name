7a6799a74c156656048a90a83158bb4479f7f6b1
murilinhops <murilinhorps@gmail.com>
2022-06-25 14:54:11 -0300

    Adjust EnemySpawn method check for null and Add method to set cameraConfiner

--- Assets/Scripts/LevelDesigns.cs
+++ Assets/Scripts/LevelDesigns.cs
@@ -1,8 +1,11 @@
 using System.Linq;
 using UnityEngine;
+using Cinemachine;
 
 public class LevelDesigns : Singleton<LevelDesigns>
 {
+    public Collider2D[] LevelCameraConfiners;
+    [SerializeField] private CinemachineConfiner CameraConfiner;
     public GameObject[] Levels;
     [ReadOnly, SerializeField] private int MaxRounds;
     public int getMaxRounds => MaxRounds;
@@ -12,19 +15,23 @@ public class LevelDesigns : Singleton<LevelDesigns>
         MaxRounds = Levels.Length - 1;
     }
 
+    public void SetCameraConfiner(int index = 0) => CameraConfiner.m_BoundingShape2D = LevelCameraConfiners[index];
+
     public Vector3 GetPlayerSpawn(int index = 0)
     {
         var allTransforms = Levels[index].GetComponentsInChildren<Transform>();
-        var spawnTransform = allTransforms.Where(k => k.name == "SpawnPlayer").First();
+        var spawnTransform = allTransforms.Where(k => k.name == "SpawnPlayer").FirstOrDefault();
 
         return spawnTransform.position;
     }
 
-    public void DestroyEnemySpawn(int index)
+    public void SetActiveEnemySpawn(int index, bool setActive)
     {
         var allEnemySpawns = Levels[index].GetComponentsInChildren<Transform>();
-        var enemySpawn = allEnemySpawns.Where(k => k.name == "SpawnArea").First();
+        var enemySpawn = allEnemySpawns.Where(k => k.name.Contains("SpawnArea")).FirstOrDefault();
 
-        enemySpawn.GetComponent<LevelBounds>().enabled = false;
+        if (enemySpawn)
+            enemySpawn.GetComponent<LevelBounds>().enabled = setActive;
+
     }
 }
